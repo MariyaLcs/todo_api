2075ef37d2e5e007392d8a7945c63ae2e1269f38
Mariya <mariya.podosinova@gmail.com>
2020-05-09 11:14:03 +0100

    DB set up

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,20 @@
-//name
+var mongoose = require("mongoose");
 
-//completed
+var todoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "Name cannot be blank!",
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-//creadted
+var Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
