a65645a4533ece779aa5fe5828365f9304289060
Mariya <mariya.podosinova@gmail.com>
2020-05-09 11:28:20 +0100

    Defing the Index route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require("express"),
   app = express();
 
+var todoRoutes = require("./routes/todos");
+
 app.get("/", function (req, res) {
   res.send("HI!!!!");
 });
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server has started!");
