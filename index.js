4f3eac0ba19c6f67c213e14c8a13e98de7dc8481
Mariya <mariya.podosinova@gmail.com>
2020-05-11 15:14:18 +0100

    Show/Update Routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.send("HI!!!!");
